f3a6df47e83ffff39df56ed15543a90327f4017e
Christian Vasquez <christianvasquezmelo@gmail.com>
2018-12-05 11:44:24 -0400

    Open sign up page

---
+++ pages/home_page.py
@@ -0,0 +1,22 @@
+import strings
+from pages.page import Page
+
+from selenium.webdriver.common.by import By
+
+from libraries.pagefactory_support import callable_find_by as find_by
+from pages.sign_up_page import SignUpPage
+
+
+class HomePage(Page):
+    _button_sign_up = find_by(how=By.XPATH, using="//a[contains(text(), 'Sign Up')]")
+
+    def __init__(self, driver):
+        super(HomePage, self).__init__(driver)
+
+    def open(self):
+        self._driver.get(strings.base_url)
+
+    def go_to_sign_up_page(self):
+        self._button_sign_up().click()
+
+        return SignUpPage(self._driver)
---
+++ pages/sign_up_page.py
@@ -0,0 +1,7 @@
+from pages.page import Page
+
+
+class SignUpPage(Page):
+
+    def __init__(self, driver):
+        super(SignUpPage, self).__init__(driver)

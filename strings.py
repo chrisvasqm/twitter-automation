ffd7b937986ad9611c1eac868c6cf3523a38ff11
Christian Vasquez <christianvasquezmelo@gmail.com>
2018-12-09 13:32:37 -0400

    Change external link

--- strings.py
+++ strings.py
@@ -1,6 +1,6 @@
 twitter_url = "https://twitter.com/"
 gmail_url = "https://www.google.com/gmail/"
-external_link = "https://dev.to/chrisvasqm/watch-your-coding-manners-5ep9"
+external_link = "https://dev.to/chrisvasqm/its-just-syntactic-sugar-apo"
 
 tweet_plain_text = "tweet with plain text"
 tweet_plain_text_and_photo = "tweet with plain text and a photo"

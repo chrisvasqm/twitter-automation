4cffa84975a7d90b4718553e49b3c1a881fe3b3a
Christian Vasquez <christianvasquezmelo@gmail.com>
2018-12-09 13:48:16 -0400

    Add tweet with external link test

--- testcases/test_twitter.py
+++ testcases/test_twitter.py
@@ -47,6 +47,21 @@ class TwitterTests(unittest.TestCase):
 
         assert home_page.is_tweet_sent() is True
 
+    def test_write_a_tweet_with_a_link(self):
+        home_page = self.landing_page.login(config.email, config.password)
+        home_page.open_new_tweet_dialog()
+
+        NewTweetModal(self.driver) \
+            .add_message(strings.tweet_plain_text_and_link) \
+            .add_link(strings.external_link) \
+            .submit()
+
+        assert home_page.is_tweet_sent() is True
+
+        home_page.see_new_tweet()
+
+        assert home_page.has_link_preview() is True
+
 
 def tearDown(self):
     self.driver.quit()
